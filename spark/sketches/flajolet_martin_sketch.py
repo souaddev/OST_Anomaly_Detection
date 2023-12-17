437de5254fa1e2973c1bf57161dbefacd8cee989
oumaima <oumaima.dhaidah1sm2016@gmail.com>
2023-12-17 18:44:29 +0100

    UPDATE SKETCH

--- spark/sketches/flajolet_martin_sketch.py
+++ spark/sketches/flajolet_martin_sketch.py
@@ -1,129 +1,37 @@
-import logging
-import os
-import pyspark
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from flajolet_martin import FlajoletMartinSketch
+from spark_processor import SparkDataProcessor
+from flajolet_martin2 import FlajoletMartinSketch
 
-def create_spark_connection():
-    s_conn = None
-    try:
-        s_conn = SparkSession.builder \
-            .appName("FlajoletMartinSketch") \
-            .getOrCreate()
-        s_conn.conf.set("spark.executor.memory", "2g")
-        s_conn.sparkContext.setLogLevel("ERROR")
-        logging.info("Spark connection created successfully!")
-    except Exception as e:
-        logging.error(f"Error creating Spark session: {e}")
-    return s_conn
+# Initialize SparkDataProcessor and process the data
+data_processor = SparkDataProcessor()
+data_processor.read_data_from_kafka()
+df = data_processor.get_processed_data()
 
-def connect_to_kafka(spark):
-    spark_df = None
-    try:
-        spark_df = spark \
-            .readStream \
-            .format("kafka") \
-            .option("kafka.bootstrap.servers", "localhost:9092") \
-            .option("subscribe", "swat_data") \
-            .option("startingOffsets", "earliest") \
-            .option("maxOffsetsPerTrigger", 50) \
-            .load()
-        logging.info("Connected to Kafka successfully")
-    except Exception as e:
-        logging.error(f"Error connecting to Kafka: {e}")
-    return spark_df
-def create_selection_df_from_kafka(spark_df):
+# Flajolet-Martin sketches storage
+fm_sketches = {}
 
-    schema = StructType([
-    StructField("Timestamp", StringType(), True),
-    StructField("FIT101", StringType(), True),
-    StructField("LIT101", StringType(), True),
-    StructField("MV101", StringType(), True),
-    StructField("P101", StringType(), True),
-    StructField("P102", StringType(), True),
-    StructField("AIT201", StringType(), True),
-    StructField("AIT202", StringType(), True),
-    StructField("AIT203", StringType(), True),
-    StructField("FIT201", StringType(), True),
-    StructField("MV201", StringType(), True),
-    StructField("P201", StringType(), True),
-    StructField("P202", StringType(), True),
-    StructField("P203", StringType(), True),
-    StructField("P204", StringType(), True),
-    StructField("P205", StringType(), True),
-    StructField("P206", StringType(), True),
-    StructField("DPIT301", StringType(), True),
-    StructField("FIT301", StringType(), True),
-    StructField("LIT301", StringType(), True),
-    StructField("MV301", StringType(), True),
-    StructField("MV302", StringType(), True),
-    StructField("MV303", StringType(), True),
-    StructField("MV304", StringType(), True),
-    StructField("P301", StringType(), True),
-    StructField("P302", StringType(), True),
-    StructField("AIT401", StringType(), True),
-    StructField("AIT402", StringType(), True),
-    StructField("FIT401", StringType(), True),
-    StructField("LIT401", StringType(), True),
-    StructField("P401", StringType(), True),
-    StructField("P402", StringType(), True),
-    StructField("P403", StringType(), True),
-    StructField("P404", StringType(), True),
-    StructField("UV401", StringType(), True),
-    StructField("AIT501", StringType(), True),
-    StructField("AIT502", StringType(), True),
-    StructField("AIT503", StringType(), True),
-    StructField("AIT504", StringType(), True),
-    StructField("FIT501", StringType(), True),
-    StructField("FIT502", StringType(), True),
-    StructField("FIT503", StringType(), True),
-    StructField("FIT504", StringType(), True),
-    StructField("P501", StringType(), True),
-    StructField("P502", StringType(), True),
-    StructField("PIT501", StringType(), True),
-    StructField("PIT502", StringType(), True),
-    StructField("PIT503", StringType(), True),
-    StructField("FIT601", StringType(), True),
-    StructField("P601", StringType(), True),
-    StructField("P602", StringType(), True),
-    StructField("P603", StringType(), True),
-    StructField("Normal/Attack", StringType(), True)
-])
+def process_batch(batch_df, epoch_id):
+    global fm_sketches
 
-    sel = spark_df.selectExpr("CAST(value AS STRING)") \
-        .select(from_json(col('value'), schema).alias('data')).select("data.*")
-    print(sel)
+    # Initialize Flajolet-Martin sketch for this batch
+    fm_sketch = FlajoletMartinSketch(num_perm=128)
 
-    return sel
-
-def process_batch(batch_df, epoch_id, flajolet_martin):
-    # DPIT301:  might be useful for detecting anomalies in pressure systems.
+    # Extract values from DataFrame columns
+    # Choose columns with expected high cardinality
     column_to_track = "DPIT301"  
-    items = batch_df.select(column_to_track).rdd.flatMap(lambda x: x).collect()
-    for item in items:
-        flajolet_martin.add(item)
-    
-    estimated_count = flajolet_martin.estimate_count()
+    column_values = batch_df.select(column_to_track).rdd.flatMap(lambda x: x).collect()
+    for value in column_values:
+            fm_sketch.add_value(value)
+
+ # Get the Flajolet-Martin sketch for the current batch and estimate count
+    current_sketch = fm_sketch.get_sketch()
+    estimated_count = fm_sketch.estimate_count()
     print(f"Epoch {epoch_id}: Estimated distinct count for {column_to_track} = {estimated_count}")
+    # Store the current sketch for future use or comparison
+    fm_sketches[epoch_id] = current_sketch
 
-if __name__ == "__main__":
-    spark_conn = create_spark_connection()
-    
-    if spark_conn:
-        kafka_df = connect_to_kafka(spark_conn)
-        if kafka_df:
-            kafka_df = kafka_df.selectExpr("CAST(value AS STRING)")
-            flajolet_martin = FlajoletMartinSketch()
-            
-            query = kafka_df \
-                .writeStream \
-                .outputMode("append") \
-                .format("console") \
-                .trigger(processingTime='30 seconds') \
-                .foreachBatch(lambda df, epoch_id: process_batch(df, epoch_id, flajolet_martin)) \
-                .start()
-            
-            query.awaitTermination()
+# Define the streaming query using foreachBatch
+query = df.writeStream.foreachBatch(process_batch).start()
+query.awaitTermination()
 
+# Don't forget to stop the Spark session when done
+# data_processor.stop_spark_session()
