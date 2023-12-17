89dbd7fa63b8d68425db642cfd261a7eb1f27bdd
oumaima <oumaima.dhaidah1sm2016@gmail.com>
2023-12-17 18:44:52 +0100

    UPDATE FLAJOLET MARTIN

--- spark/sketches/flajolet_martin.py
+++ spark/sketches/flajolet_martin.py
@@ -1,15 +1,25 @@
 import hashlib
+import numpy as np
 
 class FlajoletMartinSketch:
-    def __init__(self):
-        self.bit_array = [0] * 32  # Using
+    def __init__(self, num_perm=128):
+        self.num_perm = num_perm
+        self.bit_arrays = [0] * num_perm
 
-    def add(self, item):
-        binary_hash = bin(int(hashlib.sha256(str(item).encode('utf-8')).hexdigest(), 16))
-        trailing_zeros = len(binary_hash) - len(binary_hash.rstrip('0'))
-        index = min(trailing_zeros, 31)  # Cap the index at 31 for our bit array size
-        self.bit_array[index] = 1
+    def _hash_function(self, item, seed):
+        return bin(int(hashlib.sha256((str(item) + str(seed)).encode('utf-8')).hexdigest(), 16))
+
+    def _rho(self, hashed_value):
+        return len(hashed_value) - len(hashed_value.rstrip('0'))
+
+    def add_value(self, value):
+        for i in range(self.num_perm):
+            hashed_value = self._hash_function(value, i)
+            self.bit_arrays[i] = max(self.bit_arrays[i], self._rho(hashed_value))
+
+    def get_sketch(self):
+        return self.bit_arrays
 
     def estimate_count(self):
-        R = self.bit_array.index(0) if 0 in self.bit_array else 32
-        return 2 ** R
+        estimates = [2 ** rho for rho in self.bit_arrays]
+        return int(np.median(estimates))
